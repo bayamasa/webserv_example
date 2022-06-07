cc1a7a9701664c35689be957fd19f4b06e5827b5
bayamasa <mapi0213mgt@gmail.com>
2022-06-07 22:24:54 +0900

    concurrency サーバーdone

---
+++ sandbox/cserver.cpp
@@ -0,0 +1,102 @@
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 12345
+
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+
+enum {
+	NQUEUESIZE = 5,
+};
+
+char *message = "Hello!\nGood-bye!!]\\n";
+
+int main(void)
+{
+	int s, ws, soval, cc;
+	struct sockaddr_in sa, ca;
+	socklen_t ca_len;
+	
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("socket");
+		exit(1);
+	}
+	
+	soval = 1;
+	if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &soval, sizeof(soval)) == -1)
+	{
+		perror("setsockopt");
+		exit(1);
+	}
+	
+	std::memset(&sa, 0, sizeof(sa));
+	sa.sin_len = sizeof(sa);
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(SERVER_PORT);
+	sa.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+	if (bind(s, (struct sockaddr *)&sa, sizeof(sa)) == -1){
+		perror("bind");
+		exit(1);
+	}
+	if (listen(s, NQUEUESIZE)){
+		perror("listen");
+		exit(1);
+	}
+	printf("socket %d\n", s);
+	fprintf(stderr, "Ready.\n");
+	for (;;){
+		pid_t forkcc;
+		int status;
+		
+		while (waitpid(-1, &status, WNOHANG) > 0);
+		
+		fprintf(stderr, "Waiting for a connection...\n");
+		ca_len = sizeof(ca);
+		if ((ws = accept(s, (struct sockaddr *)&ca, &ca_len)) == -1)
+		{
+			perror("accept");
+			exit(1);
+		}
+		fprintf(stderr, "Connection established.\n");
+		if ((forkcc = fork()) == -1)
+		{
+			perror("fork");
+			exit(1);
+		} else if (forkcc == 0) {
+			// listen用のsocketはforkでcpされているので、closeする
+			if (close(s) == -1)
+			{
+				perror("child: close listening socket");
+				exit(1);
+			}
+			fprintf(stderr, "Sending the message...\n");
+			if ((cc = write(ws, message, strlen(message)) == -1))
+			{
+				perror("write");
+				exit(1);
+			}
+			fprintf(stderr, "Message sent.\n");
+			if (shutdown(ws, SHUT_RDWR) == -1) {
+				perror("shutdown");
+				exit(1);
+			}
+			if (close(ws) == -1){
+				perror("close");
+				exit(1);
+			}
+			exit(0);
+		}
+		// 親プロセスでのwsは不要
+		if (close(ws) == -1) {
+			perror("close");
+			exit(1);
+		}
+	}
+}
