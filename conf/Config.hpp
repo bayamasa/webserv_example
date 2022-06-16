50b8e07435f76778b59bd5ee5cc6fa7ff3d1ffa0
bayamasa <mapi0213mgt@gmail.com>
2022-06-16 23:28:24 +0900

    dir名変更

---
+++ conf/Config.hpp
@@ -0,0 +1,15 @@
+#ifndef CONFIG_HPP
+# define CONFIG_HPP
+
+class Config
+{
+private:
+	
+public:
+	Config();
+	~Config();
+	Config(const Config &other);
+	Config &operator=(const Config &other);
+};
+
+#endif
---
+++ conf/conf.hpp
@@ -0,0 +1,7 @@
+#ifndef CONF_HPP
+# define CONF_HPP
+
+#include "Config.hpp"
+#include "ConfigReader.hpp"
+
+#endif
