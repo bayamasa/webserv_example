b9efa6af392ae09ab77aee85bdc4517acc1dcfab
bayamasa <mapi0213mgt@gmail.com>
2022-07-04 12:20:46 +0900

    参照のデータを壊す例

---
+++ sandbox/classtest.cpp
@@ -0,0 +1,60 @@
+#ifndef CLASSTEST_HPP
+# define CLASSTEST_HPP
+
+#include <string>
+#include <iostream>
+
+class classtest
+{
+private:
+	std::string _data;
+public:
+	classtest();
+	~classtest();
+	classtest(const classtest &other);
+	classtest &operator=(const classtest &other);
+	void SetData(std::string data);
+	std::string &GetData();
+};
+
+classtest::classtest()
+{
+}
+classtest::~classtest()
+{
+}
+classtest::classtest(const classtest &other)
+{
+	*this = other;
+}
+classtest &classtest::operator=(const classtest &other)
+{
+	if (this != &other)
+	{
+	}
+	return *this;
+}
+
+void classtest::SetData(std::string data)
+{
+	_data = data;
+}
+
+std::string &classtest::GetData()
+{
+	return _data;
+}
+
+
+int main(void)
+{
+	classtest c;
+	c.SetData("helllo");
+	std::string d = c.GetData();
+	std::string hi = "hi";
+	c.GetData().swap(hi);
+	std::cout << d << std::endl;
+	std::cout << c.GetData() << std::endl;
+}
+#endif
+
